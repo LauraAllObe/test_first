6d52617685c8ecbac18a98702fc45d2cabcf96f7
Jaidynsellers <162491659+Jaidynsellers@users.noreply.github.com>
2024-03-07 13:25:33 -0500

    Update guess_the_number.py

--- guess_the_number.py
+++ guess_the_number.py
@@ -2,13 +2,15 @@ import random
 
 def guess_the_number(range_limit, guess):
     if not isinstance(range_limit, int) or not isinstance(guess, int):
-        return "Invalid input: range and guess must be integers"
-    if range_limit <= 0 or range_limit > 10000:
-        return "Invalid input: range must be a positive integer <= 10000"
+        return "Invalid input: must be an integer"
+    if range_limit <= 0:
+        return "Invalid input: must be a positive number"
+    if range_limit > 10000:
+        return "Invalid input: must be a number <=10000"
     if guess <= 0:
-        return "Invalid input: guess must be a positive integer"
+        return "Invalid input: must be a positive number"
     if guess > range_limit:
-        return "Invalid input: guess must be within the range"
+        return "Invalid input: the guess must be within the range"
     
     number_to_guess = random.randint(1, range_limit)
     
