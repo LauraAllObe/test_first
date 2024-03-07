cce4e9da02c81ba7d296fcf5a9b379b1a59d2fd3
Laura Obermaier <lauraaobermaier@gmail.com>
2024-03-07 13:43:29 -0500

    fixing errors

--- guess_the_number.py
+++ guess_the_number.py
@@ -18,17 +18,3 @@ def guess_the_number(range_limit, guess):
         return "CONGRATULATIONS!! YOU GUESSED CORRECT!"
     else:
         return "womp womp."
-
-
-
-  """
-  please read:
-  use random.randint(1, range) for random number please (so the mock in the unit test works)
-  returns (to be compatible with unit tests):
-  guess_the_number(1.5,1) should return "Invalid input: must be an integer"
-  guess_the_number(-100,1) should return "Invalid input: must be a positive number"
-  guess_the_number(1000000,1) should return "Invalid input: must be a number <=10000"
-  guess_the_number(100,1000) should return "Invalid input: the guess must be within the range"
-  guess_the_number(100,5) should return "CONGRATULATIONS!! YOU GUESSED CORRECT!"
-  guess_the_number(100,5) should return "womp womp."
-  """
--- test_guess_the_number.py
+++ test_guess_the_number.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import patch
 from guess_the_number import guess_the_number
 
 class test_guess_the_number(unittest.TestCase):
@@ -11,11 +12,11 @@ class test_guess_the_number(unittest.TestCase):
   def test_out_of_range(self):
     self.assertEqual(guess_the_number(100,1000), "Invalid input: the guess must be within the range")
   @patch('random.randint')
-  def test_correct_guess(self):
+  def test_correct_guess(self, mock_randint):
     mock_randint.return_value = 5
     self.assertEqual(guess_the_number(100,5), "CONGRATULATIONS!! YOU GUESSED CORRECT!")
   @patch('random.randint')
-  def test_incorrect_guess(self):
+  def test_incorrect_guess(self, mock_randint):
     mock_randint.return_value = 10
     self.assertEqual(guess_the_number(100,5), "womp womp.")
   
