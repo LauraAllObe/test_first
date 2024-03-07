75e729e1acd247f7960564c33897100d9f55e971
Laura <126280324+LauraAllObe@users.noreply.github.com>
2024-03-07 12:05:53 -0500

    Update test_guess_the_number.py

--- test_guess_the_number.py
+++ test_guess_the_number.py
@@ -2,5 +2,17 @@ import unittest
 from guess_the_number import guess_the_number
 
 class test_guess_the_number(unittest.TestCase):
-  #code here
+  def test_non_integer(self):
+    self.assertEqual(guess_the_number(1.5,1),"Invalid input: must be an integer")
+  def test_lower_bound(self):
+    self.assertEqual(guess_the_number(-100,1), "Invalid input: must be a positive number")
+  def test_upper_bound(self):
+    self.assertEqual(guess_the_number(1000000,1), "Invalid input: must be a number <=10000")
+  def test_out_of_range(self):
+    self.assertEqual(guess_the_number(100,1000), "Invalid input: the guess must be within the range")
+  def test_correct_guess(self):
+    self.assertEqual(guess_the_number(100,5), "CONGRATULATIONS!! YOU GUESSED CORRECT!")#add mock
+  def test_incorrect_guess(self):
+    self.assertEqual(guess_the_number(100,5), "womp womp.")#add mock
+  
   
